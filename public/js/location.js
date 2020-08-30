e798ead80a2cfc580e8798638f289ddd49bfd492
molly soufrine <mollysoufrine@gmail.com>
2020-08-30 18:19:12 -0400

    updated map to use google api and weather api for location of res. using ajax call and dom manipulation. Made ajax call for DB to get location of res by lat and lng

---
+++ public/js/location.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+  //ajax call to DB where the location of res or res's will be stored.
+  $(function() {
+    $(
+      ".resName".on("click", function(event) {
+        var lat = $(this).data("lat");
+        var lng = $(this).data("lng");
+        $.ajax("/reslocation/" + lat + lng, {
+          method: "GET",
+          data: {
+            location: true,
+          },
+        })
+          .then(function() {
+            console.log("this res has been located!");
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      })
+    );
+  });
+
+  var lat = 41.161563; //supply by DB
+  var lng = -73.417751;
+  ("use strict");
+  // In this example, we center the map, and add a marker, using a LatLng object
+  // literal instead of a google.maps.LatLng object. LatLng object literals are
+  // a convenient way to add a LatLng coordinate and, in most cases, can be used
+  // in place of a google.maps.LatLng object.
+  let map;
+  function initMap() {
+    const mapOptions = {
+      zoom: 8,
+      center: {
+        lat: lat,
+        lng: lng,
+      },
+    };
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    const marker = new google.maps.Marker({
+      // The below line is equivalent to writing:
+      // position: new google.maps.LatLng("", "")
+      position: {
+        lat: lat,
+        lng: lng,
+      },
+      map: map,
+    }); // You can use a LatLng literal in place of a google.maps.LatLng object when
+    // creating the Marker object. Once the Marker object is instantiated, its
+    // position will be available as a google.maps.LatLng object. In this case,
+    // we retrieve the marker's position using the
+    // google.maps.LatLng.getPosition() method.
+    const infowindow = new google.maps.InfoWindow({
+      content: "<p>Marker Location:" + marker.getPosition() + "</p>",
+    });
+    google.maps.event.addListener(marker, "click", () => {
+      infowindow.open(map, marker);
+    });
+  }
+  initMap();
+  $.ajax({
+    url: `https://weather.ls.hereapi.com/weather/1.0/report.json?product=observation&latitude=${lat}&longitude=${lng}&oneobservation=true&apiKey=SzqNUpgamzSYn-wUIvDfej8CKS8h8gH1XbxBy17TkVA`,
+    success: function(data) {
+      console.log(data);
+      $("#city").text(
+        "This restaurant is located in " +
+          data.observations.location[0].observation[0].city
+      );
+      $("#temp").text(
+        "The current temperature is " +
+          data.observations.location[0].observation[0].temperature +
+          " degrees Fahrenheit"
+      );
+      $("#humidity").text(
+        "The Humidity is " +
+          data.observations.location[0].observation[0].humidity +
+          "%"
+      );
+      //has to change to manipulate the DOM, use jquery to show weather
+
+      $("#skydescription").text(
+        data.observations.location[0].observation[0].description
+      );
+      $("#wind").text(
+        "The wind speed today is " +
+          data.observations.location[0].observation[0].windSpeed
+      );
+    },
+  });
+});
--- routes/reslocation-api-route.js
+++ routes/reslocation-api-route.js
@@ -7,11 +7,3 @@ const { request } = require("http");
 const { json } = require("body-parser");
 
 router.use(bodyParser.urlencoded({ extend: true }));
-let map;
-
-router.get("/api/restaurant", function(req,res){
-    db.Restaurant.findAll({}).then(function(dbRestaurant){
-        res.json(dbRestaurant)
-    })
-}
-
