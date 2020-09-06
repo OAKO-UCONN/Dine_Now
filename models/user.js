444da72ac212d61e113b3cb6a2d1a074dde8e7d6
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-09-06 15:55:59 -0400

    Fixes

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 const bcrypt = require("bcryptjs");
 // Creating our User model
@@ -10,8 +11,7 @@ module.exports = function(sequelize, DataTypes) {
       unique: true,
       validate: {
         isEmail: true,
-    
-    },
+      },
     // The password cannot be null
     password: {
       type: DataTypes.STRING,
@@ -20,8 +20,8 @@ module.exports = function(sequelize, DataTypes) {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-  });
+    }
+  }})
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
