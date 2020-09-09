480ed14fe9e94eb8d16f54b046b78ae098a6c3b2
molly soufrine <mollysoufrine@gmail.com>
2020-09-09 15:58:03 -0400

    tried to redirct the authentication route, after loggin in, it'd take you to restaurnts.html instead of members but it's not working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -32,15 +32,15 @@ module.exports = function(app) {
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/restaurants");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/restaurant ", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/restaurant.html"));
+  app.get("/restaurants", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/restaurants.html"));
   });
 
   app.get("/members", (req, res) => {
