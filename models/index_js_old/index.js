4177adf7bac01c619a88e2bb506a3091efa7ede2
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-09-08 20:33:56 -0400

    debug

---
+++ models/index_js_old/index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var basename = path.basename(module.filename);
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.json")[env];
+var db = {};
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter(function(file) {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach(function(file) {
+    var model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.Table.belongsTo(db.User, {
+  foreignKey: "user_id",
+});
+
+db.Table.belongsTo(db.Restaraunt, {
+  foreignKey: "restaraunt_id",
+});
+
+module.exports = db;
